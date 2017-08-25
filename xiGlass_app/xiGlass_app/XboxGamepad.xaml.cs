df176c1cdf9c1abc7e5eb515e687c635255584dd
Mr. El <elanebritton96@gmail.com>
2017-08-25 17:48:35 -0400

    changed positioning
    of buttons
    And added experimental code for joysticks.

--- xiGlass_app/xiGlass_app/XboxGamepad.xaml.cs
+++ xiGlass_app/xiGlass_app/XboxGamepad.xaml.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Linq;
+using SlimDX.DirectInput;
 
 namespace xiGlass_app
 {
@@ -31,23 +32,50 @@ namespace xiGlass_app
             this.Hide();
         }
 
-        public class CanvasAutoSize : Canvas
+        //Experimental code for joysticks
+        DirectInput input = new DirectInput();
+        private JoystickState state;
+
+        public class GameController
         {
-            protected override System.Windows.Size MeasureOverride(System.Windows.Size constraint)
-            {
-                base.MeasureOverride(constraint);
-                double width = base
-                    .InternalChildren
-                    .OfType<UIElement>()
-                    .Max(i => i.DesiredSize.Width + (double)i.GetValue(Canvas.LeftProperty));
-
-                double height = base
-                    .InternalChildren
-                    .OfType<UIElement>()
-                    .Max(i => i.DesiredSize.Height + (double)i.GetValue(Canvas.TopProperty));
-
-                return new System.Windows.Size(width, height);
-            }
+            private Joystick joystick;
+            private JoystickState state = new JoystickState();
         }
+
+        /*
+        public GameControllers(DirectInput directInput, Game game, int number)
+        {
+            var devices = directInput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly);
+            if (devices.Count == 0 || devices[number] == null)
+            {
+                // No Device
+                return;
+            }
+
+            // Create Gamepad
+            leftJoyStick = new Joystick(directInput, devices[number].InstanceGuid); //Basically WPF C# buttons are not compatible with direct input
+            leftJoyStick.SetCooperativeLevel(game.Window.Handle, CooperativeLevel.Exclusive | CooperativeLevel.Foreground);
+
+            // Set Axis Range for the Analog Sticks between -1000 and 1000 
+            foreach (DeviceObjectInstance deviceObject in leftJoyStick.GetObjects())
+            {
+                if ((deviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                    leftJoyStick.GetObjectPropertiesById((int)deviceObject.ObjectType).SetRange(-1000, 1000);
+            }
+            leftJoyStick.Acquire();
+        }
+
+        public JoystickState GetState()
+        {
+            if (leftJoyStick.Acquire().IsFailure || leftJoyStick.Poll().IsFailure)
+            {
+                state = new JoystickState();
+                return state;
+            }
+
+            state = leftJoyStick.GetCurrentState();
+
+            return state;
+        }*/
     }
 }
